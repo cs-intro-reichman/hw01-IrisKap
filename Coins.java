443c9d91ebc958a9e22c9c4be40d2cba7934097b
Iris Kaplun <iris.kaplun@post.runi.ac.il>
2023-12-20 15:05:26 +0200

    homework submittion

--- Coins.java
+++ Coins.java
@@ -1,9 +1,10 @@
-/*
- * Write a program that gets a quantity of cents as a command-line argument.
- * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
- */
-public class Coins {
+public class Coins { 
 	public static void main(String[] args) {
-		// Put your code here
-	}
+
+		int n = Integer.parseInt(args[0]);
+
+		int quarters = n / 25 ;
+		int cents = n % 25 ;
+		System.out.println("Use "+ quarters +" quarters and "+ cents + " cents"); 
+	} 
 }
--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,27 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
-public class GenThree {
+public class GenThree { 
+	
 	public static void main(String[] args) {
-		// Put your code here	
-	}
+
+	int min = Integer.parseInt(args[0]); 
+	int max = Integer.parseInt(args[1]);  
+
+	double random1 = Math.random();
+	int num1 = (int)(random1 * (max - min)) + min;
+	System.out.println(num1);
+
+	double random2 = Math.random();
+	int num2 = (int)(random2 * (max - min)) + min;
+	System.out.println(num2);
+
+
+	double random3 = Math.random();
+	int num3 = (int)(random3 * (max - min)) + min;
+	System.out.println(num3);
+
+
+	int minimalNum = Math.min(num1,num2);
+	minimalNum = Math.min(minimalNum,num3);
+		
+		System.out.println("The minimal generated number was " + minimalNum);  
+	} 
 }
